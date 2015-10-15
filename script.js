cc535ff8ec7878f1b1804053514530c1afbaf8d6
darkdanito <darkdanito@hotmail.com>
2015-10-15 22:47:23 +0800

    Version 2.0
    - Modded the way the GUI work as discussed with JH.
    - Old files are in the folder for reference

--- script.js
+++ script.js
@@ -1,21 +1,72 @@
-// Code goes here
-
-var mainApp = angular.module("mainApp", ['ngRoute']);
- 
-mainApp.config(function($routeProvider) 
+var app = angular.module('my-app', [], function () 
 {
-    $routeProvider
-        .when('/home', 
+})
+
+app.controller('AppController', function ($scope) 
+{
+    $scope.toggle = true;
+	$scope.currentLocation = '';
+	$scope.destination = '';
+
+/*  
+	if($scope.toggle.length)
+	{
+		$scope.toggle = 'if statement';
+	}else
+	{
+		$scope.toggle = 'else statement';
+		$scope.destination = 'aaa';
+	}
+
+	$scope.$watch('toggle', function()
+	{
+        $scope.toggleText = $scope.toggle ? 'Toggle!' : 'some text 2';
+    })
+*/
+	$scope.$watch('boonLay', function()
+	{
+//		$scope.destination = $scope.boonLay ? '' : 'Boon Lay';
+//		$scope.station = $scope.boonLay ? '' : 'Boon Lay'; 
+		
+		if(!$scope.currentLocation.length && !$scope.destination.length)
 		{
-            templateUrl: 'home.html',
-            controller: 'StudentController'
-        })
-        .when('/view1', 
+			$scope.currentLocation = $scope.boonLay ? '' : 'Boon Lay'; 
+		}else
 		{
-            templateUrl: 'view1.html',
-            controller: 'StudentController'
-        })
-        .otherwise({
-            redirectTo: '/home'
-        });
-});
+			$scope.destination = $scope.boonLay ? '' : 'Boon Lay';
+		}
+		
+    })
+	
+	$scope.$watch('lakeside', function()
+	{
+//      $scope.currentLocation = $scope.lakeside ? '' : 'Lakeside';
+//		$scope.station = $scope.lakeside ? '' : 'Lakeside';
+
+		if(!$scope.currentLocation.length && !$scope.destination.length)
+		{
+			$scope.currentLocation = $scope.lakeside ? '' : 'Lakeside'; 
+		}else
+		{
+			$scope.destination = $scope.lakeside ? '' : 'Lakeside';
+		}
+    })
+	
+	$scope.$watch('chineseGarden', function()
+	{
+		if(!$scope.currentLocation.length && !$scope.destination.length)
+		{
+			$scope.currentLocation = $scope.chineseGarden ? '' : 'Chinese Garden'; 
+		}else
+		{
+			$scope.destination = $scope.chineseGarden ? '' : 'Chinese Garden';
+		}
+    })
+	
+	$scope.$watch('reset', function()
+	{
+		$scope.currentLocation = '';
+		$scope.destination = '';
+    })
+		
+})
