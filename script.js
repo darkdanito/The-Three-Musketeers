262204e7f06af20f5259a02ac8fd419d529f5b07
darkdanito <darkdanito@hotmail.com>
2015-10-16 00:39:52 +0800

    Version 2.3
    - Updated to do list
    - Refactor codes
    - Added comments
    - Shifted the nav bar abit

--- script.js
+++ script.js
@@ -8,26 +8,8 @@ app.controller('AppController', function ($scope)
 	$scope.currentLocation = '';
 	$scope.destination = '';
 
-/*  
-	if($scope.toggle.length)
-	{
-		$scope.toggle = 'if statement';
-	}else
-	{
-		$scope.toggle = 'else statement';
-		$scope.destination = 'aaa';
-	}
-
-	$scope.$watch('toggle', function()
-	{
-        $scope.toggleText = $scope.toggle ? 'Toggle!' : 'some text 2';
-    })
-*/
 	$scope.$watch('boonLay', function()
-	{
-//		$scope.destination = $scope.boonLay ? '' : 'Boon Lay';
-//		$scope.station = $scope.boonLay ? '' : 'Boon Lay'; 
-		
+	{	
 		if(!$scope.currentLocation.length && !$scope.destination.length)
 		{
 			$scope.currentLocation = $scope.boonLay ? '' : 'Boon Lay'; 
@@ -40,9 +22,6 @@ app.controller('AppController', function ($scope)
 	
 	$scope.$watch('lakeside', function()
 	{
-//      $scope.currentLocation = $scope.lakeside ? '' : 'Lakeside';
-//		$scope.station = $scope.lakeside ? '' : 'Lakeside';
-
 		if(!$scope.currentLocation.length && !$scope.destination.length)
 		{
 			$scope.currentLocation = $scope.lakeside ? '' : 'Lakeside'; 
